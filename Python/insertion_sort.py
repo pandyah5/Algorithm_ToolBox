a218a2fcfa6f102bbbddec255c6cef4e763fd280
pandyah5 <pandyahetav1@gmail.com>
2021-07-28 08:33:35 -0400

    Improved insertion sort

--- Python/insertion_sort.py
+++ Python/insertion_sort.py
@@ -1,15 +1,11 @@
 def insertion_sort(array):
-    if (len(array) <= 1):
-        return array
-    for index in range(0, len(array) - 1):
-        if (array[index + 1] < array[index]):
-            j = index
-            while(j >= 0):
-                if (array[j + 1] >= array[j]):
-                    break
-                array[j + 1], array[j] = array[j], array[j + 1]
-                j -= 1
-
+    for index in range(0, len(array)):
+        key = array[index]
+        j = index - 1
+        while(j >= 0 and key < array[j]):
+            array[j + 1], array[j] = array[j], array[j + 1]
+            j -= 1
+        array[j + 1] = key
     return array
 
 ## Stress Testing
@@ -21,10 +17,11 @@ def check(array):
             return False
     return True
 
-for j in range(0, 10):
+for j in range(0, 100):
     mylist = []
-    for i in range(0, 100):
+    for i in range(0, 50):
         mylist.append(np.random.randint(100))
 
     if not check(insertion_sort(mylist)):
         print("> Failed: ", mylist)
+        
