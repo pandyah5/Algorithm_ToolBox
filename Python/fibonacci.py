a6ff3737be132ea65832994d99d410324431788d
pandyah5 <pandyahetav1@gmail.com>
2021-07-30 08:26:23 -0400

    Added fibonacci function

---
+++ Python/fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(num):
+    if (num <= 2):
+        return 1
+    if (num < 1):
+        return 0
+    num1, num2, num = 1, 1, num - 2
+    while(num):
+        num2, num1 = num1, num1 + num2
+        num -= 1
+    return num1
+
+## Testing
+for i in range(1, 11):
+    print(fibonacci(i))
